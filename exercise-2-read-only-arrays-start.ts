0bcbb4c8f0c49d642bae22d3b53a2efef926801a
Nicola Pisani <pisani_nicola@yahoo.com>
2023-08-21 16:48:39 +0200

    exercise-12

---
+++ exercise-2-read-only-arrays-start.ts
@@ -0,0 +1,37 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Prevent the array from being modified.
+// Use the generic interface type `ReadonlyArray`: ReadonlyArray<type>
+// This will cause a type error. Remove the code that now has a type error.
+
+const languages: ReadonlyArray<string> = [
+  "Mongolian",
+  "French",
+  "Basque",
+  "Thai",
+];
+
+// languages[2] = "Hausa"; -----> Rimuovo questa riga per evitare la modifica dell'array.
+
+console.log(languages);
+
+// Prevent the array from being modified.
+// Use the readonly modifier and the array literal type syntax: readonly type[]
+// This will cause a type error. Remove the code that now has a type error.
+
+const currencies: readonly string[] = [
+  "NZD",
+  "THB",
+  "NGN",
+  "EUR",
+  "COP",
+  "INR",
+];
+
+//currencies.reverse(); -----> Rimuovo questa riga per evitare la modifica dell'array.
+
+console.log(currencies);
+
+export {};
